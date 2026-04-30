1ea013f8da0cf8a2f660a3e26f548b9dd125b2f5
JayKerb <kerbcyclect@gmail.com>
2026-04-30 03:51:49 -0400

    style: fix PHPCS formatting in Capabilities helper

--- includes/Helpers/Capabilities.php
+++ includes/Helpers/Capabilities.php
@@ -3,7 +3,7 @@
 namespace Kerbcycle\QrCode\Helpers;
 
 if (!defined('ABSPATH')) {
-    exit;
+	exit;
 }
 
 /**
@@ -11,34 +11,34 @@ if (!defined('ABSPATH')) {
  */
 class Capabilities
 {
-    public const MANAGE_OPERATIONS = 'kerbcycle_manage_operations';
-    public const MANAGE_SETTINGS = 'kerbcycle_manage_settings';
-    public const VIEW_LOGS = 'kerbcycle_view_logs';
+	public const MANAGE_OPERATIONS = 'kerbcycle_manage_operations';
+	public const MANAGE_SETTINGS = 'kerbcycle_manage_settings';
+	public const VIEW_LOGS = 'kerbcycle_view_logs';
 
-    public static function manage_operations()
-    {
-        return self::MANAGE_OPERATIONS;
-    }
+	public static function manage_operations()
+	{
+		return self::MANAGE_OPERATIONS;
+	}
 
-    public static function manage_settings()
-    {
-        return self::MANAGE_SETTINGS;
-    }
+	public static function manage_settings()
+	{
+		return self::MANAGE_SETTINGS;
+	}
 
-    public static function view_logs()
-    {
-        return self::VIEW_LOGS;
-    }
+	public static function view_logs()
+	{
+		return self::VIEW_LOGS;
+	}
 
-    /**
-     * Backward-compatible capability check.
-     *
-     * @param string $capability Capability to check.
-     *
-     * @return bool
-     */
-    public static function can($capability)
-    {
-        return current_user_can($capability) || current_user_can('manage_options');
-    }
+	/**
+	 * Backward-compatible capability check.
+	 *
+	 * @param string $capability Capability to check.
+	 *
+	 * @return bool
+	 */
+	public static function can($capability)
+	{
+		return current_user_can($capability) || current_user_can('manage_options');
+	}
 }
