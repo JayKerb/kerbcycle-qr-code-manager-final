9f4cc49e963cc59ecc08b6b273b1b3290ed873af
JayKerb <kerbcyclect@gmail.com>
2026-04-30 03:46:38 -0400

    style: apply PHPCS formatting in RedirectsPage

--- includes/Admin/Pages/RedirectsPage.php
+++ includes/Admin/Pages/RedirectsPage.php
@@ -2,8 +2,8 @@
 
 namespace Kerbcycle\QrCode\Admin\Pages;
 
-if (!defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
 /**
@@ -13,27 +13,24 @@ if (!defined('ABSPATH')) {
  * @package    Kerbcycle\QrCode
  * @subpackage Kerbcycle\QrCode\Admin\Pages
  */
-class RedirectsPage
-{
-    /**
-     * Redirect to the Bookly appointments page.
-     *
-     * @since    1.0.0
-     */
-    public function bookly_appointments()
-    {
-        wp_redirect(admin_url('admin.php?page=bookly-appointments'));
-        exit;
-    }
+class RedirectsPage {
+	/**
+	 * Redirect to the Bookly appointments page.
+	 *
+	 * @since    1.0.0
+	 */
+	public function bookly_appointments() {
+		wp_redirect( admin_url( 'admin.php?page=bookly-appointments' ) );
+		exit;
+	}
 
-    /**
-     * Redirect to the TeraWallet page.
-     *
-     * @since    1.0.0
-     */
-    public function terawallet()
-    {
-        wp_redirect(admin_url('admin.php?page=woo-wallet'));
-        exit;
-    }
+	/**
+	 * Redirect to the TeraWallet page.
+	 *
+	 * @since    1.0.0
+	 */
+	public function terawallet() {
+		wp_redirect( admin_url( 'admin.php?page=woo-wallet' ) );
+		exit;
+	}
 }
