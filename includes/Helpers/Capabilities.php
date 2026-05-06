c84b800d3fafba91286a12957537344f3875b30a
JayKerb <kerbcyclect@gmail.com>
2026-05-06 07:53:20 +0000

    PHPCBF: format Capabilities helper

--- includes/Helpers/Capabilities.php
+++ includes/Helpers/Capabilities.php
@@ -2,31 +2,28 @@
 
 namespace Kerbcycle\QrCode\Helpers;
 
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 /**
  * Centralized capability names and compatibility checks.
  */
-class Capabilities
-{
-	public const MANAGE_OPERATIONS = 'kerbcycle_manage_operations';
-	public const MANAGE_SETTINGS = 'kerbcycle_manage_settings';
-	public const VIEW_LOGS = 'kerbcycle_view_logs';
+class Capabilities {
 
-	public static function manage_operations()
-	{
+	public const MANAGE_OPERATIONS = 'kerbcycle_manage_operations';
+	public const MANAGE_SETTINGS   = 'kerbcycle_manage_settings';
+	public const VIEW_LOGS         = 'kerbcycle_view_logs';
+
+	public static function manage_operations() {
 		return self::MANAGE_OPERATIONS;
 	}
 
-	public static function manage_settings()
-	{
+	public static function manage_settings() {
 		return self::MANAGE_SETTINGS;
 	}
 
-	public static function view_logs()
-	{
+	public static function view_logs() {
 		return self::VIEW_LOGS;
 	}
 
@@ -37,8 +34,7 @@ class Capabilities
 	 *
 	 * @return bool
 	 */
-	public static function can($capability)
-	{
-		return current_user_can($capability) || current_user_can('manage_options');
+	public static function can( $capability ) {
+		return current_user_can( $capability ) || current_user_can( 'manage_options' );
 	}
 }
