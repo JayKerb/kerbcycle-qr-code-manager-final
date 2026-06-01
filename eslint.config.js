242c1cac17c411bdf777c1f7d13f37c344e645f5
JayKerb <kerbcyclect@gmail.com>
2026-06-01 15:09:51 +0000

    Add report-only ESLint setup

---
+++ eslint.config.js
@@ -0,0 +1,67 @@
+const globals = require( 'globals' );
+const wordpress = require( '@wordpress/eslint-plugin' );
+
+module.exports = [
+	{
+		ignores: [
+			'node_modules/**',
+			'vendor/**',
+			'build/**',
+			'dist/**',
+			'coverage/**',
+			'*.min.js',
+			'**/*.min.js',
+		],
+	},
+
+	{
+		files: [
+			'assets/js/**/*.js',
+			'admin/js/**/*.js',
+			'includes/**/*.js',
+		],
+
+		plugins: {
+			'@wordpress': wordpress,
+		},
+
+		languageOptions: {
+			ecmaVersion: 2021,
+			sourceType: 'script',
+			globals: {
+				...globals.browser,
+				...globals.jquery,
+
+				wp: 'readonly',
+				ajaxurl: 'readonly',
+				kerbcycleQrScanner: 'readonly',
+				KC_OSRM: 'readonly',
+				Html5Qrcode: 'readonly',
+				Html5QrcodeScanner: 'readonly',
+				BarcodeDetector: 'readonly',
+				L: 'readonly',
+			},
+		},
+
+		rules: {
+			'no-undef': 'error',
+			'no-unused-vars': [
+				'warn',
+				{
+					args: 'after-used',
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+				},
+			],
+			'no-console': 'off',
+			'no-alert': 'warn',
+			'no-debugger': 'error',
+			'no-dupe-keys': 'error',
+			'no-duplicate-case': 'error',
+			'no-unreachable': 'error',
+			'valid-typeof': 'error',
+			'eqeqeq': [ 'warn', 'smart' ],
+			'curly': [ 'warn', 'multi-line' ],
+		},
+	},
+];
